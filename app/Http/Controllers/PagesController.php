b923a8d7436051a794be185623292ded89b3864c
Adam Carter <adamcarter@jakatasalon.co.uk>
2015-02-15 14:55:25 +0000

    updated some stuff

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,34 @@
+<?php namespace App\Http\Controllers;
+
+class PagesController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pages Controller
+	|--------------------------------------------------------------------------
+	|
+	| This page renders the main pages of the site
+	|
+	*/
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('pages.home');
+	}
+
+}
